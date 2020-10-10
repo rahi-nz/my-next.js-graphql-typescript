0cf684b32866b28674759d292581248fff1799e3
rahi-nz <nz.rahele@gmail.com>
2020-10-10 23:02:19 +0330

    add eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,57 +1,53 @@
-import React from "react"
-import App from "next/app"
-import Head from "next/head"
-import { ThemeProvider, createGlobalStyle } from "styled-components"
-import { ApolloProvider } from "@apollo/react-hooks"
-import withApollo from "../hooks/withApollo"
-import {
-    ApolloClient,
-    InMemoryCache,
-    NormalizedCacheObject,
-} from "@apollo/client"
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
+import withApollo from "../hooks/withApollo";
 
 export interface ITheme {
-    light: string;
+  light: string;
 }
 
 export interface IThemeWrapper {
-    theme: ITheme;
+  theme: ITheme;
 }
 
 export const theme: ITheme = {
-    light: "#e8ded2",
-}
+  light: "#f1f3f4",
+};
 
 const GlobalStyle = createGlobalStyle<IThemeWrapper>`
   body {
     margin: 0 auto;
-    background: ${props => props.theme.light}; 
+    background: ${(props) => props.theme.light}; 
   }
-`
+`;
 
 interface IProps {
-    apollo: ApolloClient<NormalizedCacheObject>;
+  apollo: ApolloClient<NormalizedCacheObject>;
 }
 
 class MyApp extends App<IProps> {
-    render() {
-        const { Component, pageProps, apollo } = this.props
+  render() {
+    const { Component, pageProps, apollo } = this.props;
 
-        return (
-            <React.Fragment>
-                <Head>
-                    <title>GraphQL Job Board</title>
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
-                </Head>
-                <ApolloProvider client={apollo}>
-                    <ThemeProvider theme={theme}>
-                        <GlobalStyle />
-                        <Component {...pageProps} />
-                    </ThemeProvider>
-                </ApolloProvider>
-            </React.Fragment>
-        )
-    }
+    return (
+      <>
+        <Head>
+          <title>GraphQL Job Board</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <ApolloProvider client={apollo}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </ApolloProvider>
+      </>
+    );
+  }
 }
 
-export default withApollo(MyApp)
+export default withApollo(MyApp);
